3566b6b9b57818f208ce48b7ab2260fd09558c14
Жуков Александр <zhukov.a@autostels.tech>
2024-10-11 22:00:54 +0300

    hw4 fix source

--- src/data_clean.py
+++ src/data_clean.py
@@ -1,5 +1,42 @@
 import pyspark.sql.functions as F
-from tools import *
+import findspark
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql.types import *
+
+hdfsdir = '/user/data'
+target_S3 = 's3a://hw3-data-cleaning/'
+INPUT_BUCKET = "otus-hw-bucket"
+RESULT_BUCKET = "hw3-data-cleaning"
+
+def get_spark(app_name: str = "otus-hw"):
+    findspark.init()
+    findspark.find()
+    conf = (
+        SparkConf().setMaster("yarn").setAppName(app_name)
+        .set("spark.executor.memory", "8g")
+        .set("spark.driver.memory", "4g")
+        .set("spark.sql.execution.arrow.pyspark.enabled", "true")
+        .set("spark.executor.instances", 4)
+        .set("spark.executor.cores", 2)
+        .set('spark.sql.repl.eagerEval.enabled', True)
+    )
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    return spark
+
+
+def get_schema() -> StructType:
+    return StructType([
+        StructField("transaction_id", LongType(), True),
+        StructField("tx_datetime", TimestampType(), True),
+        StructField("customer_id", LongType(), True),
+        StructField("terminal_id", LongType(), True),
+        StructField("tx_amount", DoubleType(), True),
+        StructField("tx_time_seconds", LongType(), True),
+        StructField("tx_time_days", LongType(), True),
+        StructField("tx_fraud", LongType(), True),
+        StructField("tx_fraud_scenario", LongType(), True)
+    ])
 
 def clean_dataframe(df):
     df = df.na.drop("any")  # очистка пустых значений
@@ -11,12 +48,13 @@ def main():
     spark = get_spark()
     schema = get_schema()
 
-    filelist = ['2019-09-21']
+    filelist = ['2019-11-20']
     for filename in filelist:
         df = spark.read.schema(schema) \
             .option("comment", "#") \
             .option("header", False) \
-            .csv(f'{hdfsdir}/{filename}.txt')
+            .csv(f's3a://{INPUT_BUCKET}/{filename}')
+            # .csv(f'{hdfsdir}/{filename}.txt')
         df = clean_dataframe(df)
         # write to one parquet
         (
