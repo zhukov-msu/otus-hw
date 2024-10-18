509547d974fe4cfff5923db533c39dbb521f6743
Жуков Александр <zhukov.a@autostels.tech>
2024-10-18 18:19:40 +0300

    hw5 run model fit

--- dags/run_model_fit.py
+++ dags/run_model_fit.py
@@ -1,19 +1,25 @@
 from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.providers.ssh.operators.ssh import SSHOperator
+from datetime import datetime
+from airflow.operators.bash import BashOperator
 
 with DAG(
         dag_id = 'RUN_MODEL_FIT',
-        schedule_interval='@hourly',
+        schedule_interval='@daily',
         start_date=datetime(year = 2024,month = 1,day = 20),
         catchup=False
 ) as dag:
-    t1 = SSHOperator(
-        task_id='fit_model',
-        ssh_conn_id='mlops_ssh',
-        command="spark-submit /home/ubuntu/otus-hw/src/",
-        cmd_timeout=None,
-        remote_host="10.1.0.27",
-        key_file="/home/ubuntu/.ssh/yc_mlops",
-        username="ubuntu"
+    t1 = BashOperator(
+        task_id = 'run_model_fit',
+        bash_command = 'ssh -i /home/ubuntu/.ssh/cluster ubuntu@10.1.0.27 \
+         "spark-submit /home/ubuntu/otus-hw/src/model.py"',
     )
+
+    # t1 = SSHOperator(
+    #     task_id='fit_model',
+    #     ssh_conn_id='mlops_ssh',
+    #     command="spark-submit /home/ubuntu/otus-hw/src/",
+    #     cmd_timeout=None,
+    #     remote_host="10.1.0.27",
+    #     key_file="/home/ubuntu/.ssh/yc_mlops",
+    #     username="ubuntu"
+    # )
