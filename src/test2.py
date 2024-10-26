a79b9f59f426b79aff596e935560379dbadfb0b6
Жуков Александр <zhukov.a@autostels.tech>
2024-10-26 21:22:59 +0300

    hw6: debug experiment

---
+++ src/test2.py
@@ -0,0 +1,17 @@
+from mlflow.tracking import MlflowClient
+
+import mlflow
+import os
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "https://storage.yandexcloud.net"
+mlflow.set_tracking_uri("http://130.193.53.137:8000")
+
+experiment = mlflow.set_experiment("zhukov-test")
+experiment_id = experiment.experiment_id
+# logger.info(f"Experiment ID: {experiment_id}")
+client = MlflowClient(tracking_uri="http://130.193.53.137:8000")
+if len(client.search_runs(experiment_id, max_results=1)) < 2:
+    is_first = True
+else:
+    is_first = False
+    best_run = client.search_runs(experiment_id, order_by=["metrics.roc_auc_mean DESC"], max_results=1)[0]
